7edcbdd2fc7cc05474d553fde826ccf14087457d
yaochao <yaochaoutokyo@gmail.com>
2019-06-17 22:29:22 +0900

    init

---
+++ JsonRpcTest/src/main/java/PolveJsonRpcTest.java
@@ -0,0 +1,12 @@
+import wf.bitcoin.javabitcoindrpcclient.BitcoinJSONRPCClient;
+
+/**
+ * Created by yaochao on 2019/06/17
+ */
+public class PolveJsonRpcTest {
+	public static void main(String[] args) throws Exception {
+		String nodeUrl = "http://admin:huobijp@52.199.36.243:8332";
+		BitcoinJSONRPCClient client = new BitcoinJSONRPCClient(nodeUrl);
+		System.out.println("network info => " + client.getNetworkInfo());
+	}
+}

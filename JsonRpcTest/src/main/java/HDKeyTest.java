a2517aa0c2ae8dffbb510b0a251f8eb4dc8fc1d3
yaochao <yaochaoutokyo@gmail.com>
2019-06-18 23:21:02 +0900

    rawTx

---
+++ JsonRpcTest/src/main/java/HDKeyTest.java
@@ -0,0 +1,83 @@
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.crypto.*;
+import org.bitcoinj.params.MainNetParams;
+
+import java.security.SecureRandom;
+import java.util.List;
+
+import static org.bitcoinj.core.Utils.HEX;
+
+/**
+ * Created by yaochao on 2019/06/18
+ */
+public class HDKeyTest {
+	public static void main(String[] args) throws Exception {
+		// 生成安全随机熵源(128 bit)
+		SecureRandom random = new SecureRandom();
+		byte[] entropy = new byte[16];
+		random.nextBytes(entropy);
+		System.out.format("entropy => %s\n", HEX.encode(entropy));
+
+		// 熵转换为助记词
+		// 熵（128位） + 校验和（熵的sha256，取前4位）= 12个助记词（132/11 = 12）
+		MnemonicCode mnemonicCode = new MnemonicCode();
+		List<String> mnemonics = mnemonicCode.toMnemonic(entropy);
+		System.out.format("mnemonics => %s\n", mnemonics);
+
+		// 助记词转换为种子
+		// 助记词 + 种子（"mnemonic" + 可选的passphrase）--2048 * HMAC-SHA512--> seed
+		String passphrase = "123456";
+		byte[] seed = MnemonicCode.toSeed(mnemonics,passphrase);
+		System.out.format("seed => %s\n", HEX.encode(seed));
+
+		// 生成主密钥
+		// seed --HMAC-SHA512--> master privkey(256bit) + master chaincode(256bit)
+		DeterministicKey masterKey = HDKeyDerivation.createMasterPrivateKey(seed);
+		System.out.format("master key => %s\n", masterKey.getPrivateKeyAsHex());
+
+		// 普通衍生
+		// 使用masterKey衍生子私钥
+		DeterministicKey childPrivKey = HDKeyDerivation.deriveChildKey(masterKey, 12);
+		System.out.println("derive with master privkey:");
+		System.out.format("child privKey => %s\n", childPrivKey.getPrivateKeyAsHex());
+		System.out.format("child pubKey => %s\n", childPrivKey.getPublicKeyAsHex());
+		// 普通衍生的情况下，只需要父公钥和父链码就可以推导出指定编号的子公钥和子链码
+		// 丢掉了私钥只有公钥和链码
+		DeterministicKey masterPubKey = masterKey.dropPrivateBytes();
+		DeterministicKey childPubKey = HDKeyDerivation.deriveChildKey(masterPubKey, 12);
+		System.out.println("derive with master pubkey:");
+//		System.out.format("child privKey => %s\n", childPubKey.getPrivateKeyAsHex()); // 没有私钥
+		System.out.format("child pubKey => %s\n", childPubKey.getPublicKeyAsHex());
+
+		// 将层级密钥输出成拓展密钥，需要网络参数主网前缀为xprv和xpub，测试网为tprv和tpub
+		NetworkParameters params = MainNetParams.get();
+		String xprv = childPrivKey.serializePrivB58(params);
+		String xpub = childPrivKey.serializePubB58(params);
+		System.out.format("xprv => %s\n", xprv);
+		System.out.format("xpub => %s\n", xpub);
+		// 还可以将拓展密钥输出成层级密钥,同样需要拓展密钥和网络参数
+		DeterministicKey keyFromXprv = DeterministicKey.deserializeB58(xprv, params);
+		System.out.format("prv => %s\n", keyFromXprv.getPrivateKeyAsHex());
+		System.out.format("pub => %s\n", keyFromXprv.getPublicKeyAsHex());
+
+		// 硬化衍生
+		// 比特币根据子密钥序号来区分派生普通密钥还是强化密钥：当序号 小于0x80000000时，生成普通子密钥，否则生成强化子密钥。
+		// 注意，你需要从一个包含私钥的层级密钥才能派生强化子密钥。
+		int index = 12;
+		DeterministicKey normalKey = HDKeyDerivation.deriveChildKey(childPrivKey,index);
+		DeterministicKey hardenKey = HDKeyDerivation.deriveChildKey(childPrivKey,index | 0x80000000);
+		System.out.format("normal child privkey => %s\n", normalKey.getPrivateKeyAsHex());
+		System.out.format("harden child privkey => %s\n", hardenKey.getPrivateKeyAsHex());
+		// 还可以使用childNumber来指定是否硬化
+		DeterministicKey hardenKeyByChildNum = HDKeyDerivation.deriveChildKey(childPrivKey, new ChildNumber(index,true));
+		System.out.format("harden child privkey (child num) => %s\n", hardenKeyByChildNum.getPrivateKeyAsHex());
+
+		// 路径表示法
+		DeterministicHierarchy hierarchy = new DeterministicHierarchy(masterKey);
+		String path = "M/12/12H";
+		List<ChildNumber> numbers = HDUtils.parsePath(path);
+		DeterministicKey keyFromHDPath = hierarchy.get(numbers, false, true);
+		System.out.format("harden child privkey (path) => %s\n", keyFromHDPath.getPrivateKeyAsHex());
+		System.out.println("path =>" + keyFromHDPath.getPath());
+	}
+}
---
+++ JsonRpcTest/src/main/java/SendRawTxTest.java
@@ -0,0 +1,55 @@
+import org.bitcoinj.core.*;
+import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptBuilder;
+import wf.bitcoin.javabitcoindrpcclient.BitcoinJSONRPCClient;
+
+import static org.bitcoinj.core.Utils.HEX;
+
+/**
+ * Created by yaochao on 2019/06/18
+ */
+public class SendRawTxTest {
+	public static void main(String[] args) throws Exception {
+		// 创建3个地址，分别对应发起者，接收者和找零地址
+		ECKey senderKey = new ECKey();
+		ECKey receiverKey = new ECKey();
+		ECKey changeKey = new ECKey();
+
+		// 创建网络参数，建立交易
+		NetworkParameters params = MainNetParams.get();
+		Transaction tx = new Transaction(params);
+
+		// 添加交易输出
+		// 第一个输出，接收者地址加BTC数量
+		Coin txValue = Coin.parseCoin("5");
+		Address receiverAddr = receiverKey.toAddress(params);
+		tx.addOutput(txValue, receiverAddr);
+		// 第二个输出，找零地址加找零BTC数量，输入与输出的差值为fee(0.0001BTC)
+		Coin changeValue = Coin.parseCoin("4.9999");
+		Address changeAddr = changeKey.toAddress(params);
+		tx.addOutput(changeValue, changeAddr);
+
+		// 使用addSingedInput添加输入，其参数是交易输出点（交易id和输出序号）、 公钥脚本和签名私钥
+		// 必须先加入输出才能签名，因为签名使用的签名体是交易输出，否则会报交易无输出异常
+		String utxoTxid = "78a37b87b03101a5ff28266f01365e61122bca00c6910f8d9c5c0f450cd05c62";
+		int utxoVout = 0;
+		TransactionOutPoint outPoint = new TransactionOutPoint(params, utxoVout, Sha256Hash.wrap(utxoTxid));
+		Script utxoScript = ScriptBuilder.createOutputScript(senderKey.toAddress(params));
+		tx.addSignedInput(outPoint, utxoScript, senderKey);
+
+		// bitcoinSerialize()方法将签名的交易对象转化为字节数组，并进一步编码为16进制字符串;
+		// 这就是可以直接提交给节点旳sendrawtransaction调用的裸交易
+		String txHex = HEX.encode(tx.bitcoinSerialize());
+		System.out.format("tx hex => %s\n",txHex);
+
+		// 使用节点旳decoderawtransaction调用来检查一下裸交易的内容是否与 我们的预期一致
+		BitcoinJSONRPCClient client = new BitcoinJSONRPCClient("http://admin:huobijp@52.199.36.243:8332");
+		BitcoinJSONRPCClient.RawTransaction rawTx = client.decodeRawTransaction(txHex);
+		System.out.format("raw tx => %s\n",rawTx);
+
+		// 将交易送给节点处理,如果交易不合法会被拒绝
+		String txid = client.sendRawTransaction(txHex);
+		System.out.format("txid => %s\n", txid);
+	}
+}
